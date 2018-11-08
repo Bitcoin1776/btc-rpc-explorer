045d05e41762da8a8c2f1e98f5e0d07930f6efae
ConanDW <32853335+ConanDW@users.noreply.github.com>
2018-11-08 16:44:59 -0500

    Update coins.js

--- app/coins.js
+++ app/coins.js
@@ -1,8 +1,5 @@
-var btc = require("./coins/btc.js");
-var ltc = require("./coins/ltc.js");
+var jfk = require("./coins/jfk.js");
 
 module.exports = {
-	"BTC": btc,
-	"LTC": ltc
-	"JFK": jfk
+	"JFK": jfk,
 };
